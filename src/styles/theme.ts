f73bc12f029ecb7227fb9c8fb8909b6dcf56b34b
joaoMarinho94 <joaopaulim94@gmail.com>
2022-01-20 20:20:13 -0300

    fead: add setup chakra ui

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,41 +2,41 @@ import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
   colors: {
-    pGray: {
-      '50': '#F3F2F2',
-      '100': '#DDDCDA',
-      '200': '#C7C5C2',
-      '300': '#B1AFAA',
-      '400': '#9B9892',
-      '500': '#85817A',
-      '600': '#6A6762',
-      '700': '#504E49',
-      '800': '#353431',
-      '900': '#1B1A18',
+    gray: {
+      '900': '#181B23',
+      '800': '#1F2029',
+      '700': '#353646',
+      '600': '#4B4D63',
+      '500': '#616480',
+      '400': '#797D9A',
+      '300': '#9699B0',
+      '200': '#B3B5C6',
+      '100': '#D1D2DC',
+      '50': '#EEEEF2',
     },
   },
   fonts: {
     heading: 'Roboto',
     body: 'Roboto',
   },
-  components: {
-    Text: {
-      baseStyle: {
-        fontSize: 'lg',
-        fontWeight: 'normal',
-      },
-    },
-    Button: {
-      defaultProps: {
-        colorScheme: 'orange',
-      },
-    },
-  },
+  // components: {
+  //   Text: {
+  //     baseStyle: {
+  //       fontSize: 'lg',
+  //       fontWeight: 'normal',
+  //     },
+  //   },
+  //   Button: {
+  //     defaultProps: {
+  //       colorScheme: 'orange',
+  //     },
+  //   },
+  // },
   styles: {
     global: {
       body: {
-        bg: 'pGray.900',
-        color: 'pGray.50',
+        bg: 'gray.900',
+        color: 'gray.50',
       },
     },
   },
