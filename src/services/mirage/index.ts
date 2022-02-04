6456113681b4feb095e20b5a6247fd930658ee58
joaoMarinho94 <joaopaulim94@gmail.com>
2022-02-04 17:52:07 -0300

    feat: add pagination in get users

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,7 +1,14 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable import/no-extraneous-dependencies */
-import { createServer, Factory, Model, Registry, Server } from 'miragejs';
+import {
+  createServer,
+  Factory,
+  Model,
+  Registry,
+  Response,
+  Server,
+} from 'miragejs';
 import { AnyFactories, Assign, ModelDefinition } from 'miragejs/-types';
 import faker from 'faker';
 
@@ -46,7 +53,20 @@ export function makeServer(): Server<
       this.namespace = 'api';
       this.timing = 750;
 
-      this.get('/users');
+      this.get('/users', function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams;
+        const total = schema.all('user').length;
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all('user')).users.slice(
+          pageStart,
+          pageEnd
+        );
+
+        return new Response(200, { 'x-total-count': String(total) }, { users });
+      });
+
       this.post('/users');
 
       this.namespace = '';
